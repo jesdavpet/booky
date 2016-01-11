6515a412b33be9d928a7ade789496173698489c8
jesdavpet <jesdavpet@gmail.com>
2016-01-10 23:19:19 -0500

    JSDoc comments

--- Booky.js
+++ Booky.js
@@ -1,115 +1,168 @@
-'use strict';
+/**
+* @author Jesse Peterson [@jesdavpet]
+* @version 0.1
+* A JavaScript bookmarklet module.
+*/
 var Booky = Booky || ( function () {
+    'use strict';
 
-  function flatten( thing ) {
+    /**
+     * Flattens an nth-dimensional array of primitives, functions, and/or arrays
+     * of the same into a concatenated string value.
+     * @param {number|string|[]|function} thing - the entry point to recursively flatten
+     * @returns {string}
+     */
+    function flatten( thing ) {
     var flattened = '';
 
     switch ( wtf.type( thing ) ) {
-      case wtf.NUMBER :   // Fall through to STRING case.
-      case wtf.STRING :   // Base case.
-        flattened += thing;
-        break;
-      case wtf.FUNCTION : // Recurse on value returned.
-        flattened += flatten( thing() );
-        break;
-      case wtf.ARRAY :    // Concatenate recursed value of each element in the array.
-        for ( var t = 0; t < thing.length; t++ ) flattened += flatten( thing[t] );
-        break;
-      default : break;    // Do nothing. Non-primitives will be ignored -- including objects.
+        case wtf.NUMBER() : // Fall through to STRING case.
+        case wtf.STRING() : // Base case.
+            flattened += thing;
+            break;
+        case wtf.FUNCTION() : // Recurse on value returned.
+            flattened += flatten( thing() );
+            break;
+        case wtf.ARRAY() : // Concatenate recursed value of each element in the array.
+            for ( var t = 0; t < thing.length; t++ ) flattened += flatten( thing[t] );
+            break;
+        default : break; // Do nothing. Non-primitives will be ignored -- including objects.
     }
 
     return flattened;
-  }
-
-  function get( url, action ) {
-    action = action || function ( loc ) { window.location.href = loc; };
-
-    var location = '';
-
-    if ( wtf.is( url.base, wtf.ARRAY ) ) {
-      var bases = [];
-      for ( var b = 0; b < url.base.length; b++ ) bases.push( flatten( url.base[b] ) );
-      location += bases.join( ',' );
-    } else {
-      location += flatten( url.base );
     }
 
-    if ( url.parameters.length > 0 ) {
-      location += '?';
-      var flat = [];
-      for ( var l = 0; l < url.parameters.length; l++ ) {
-        for ( var key in url.parameters[l] ) {
-          if ( url.parameters[l].hasOwnProperty( key ) ) {
-            flat.push( key + '=' + flatten( url.parameters[l][key] ) );
-          }
+    /**
+     * Gets a mailto or http get protocol bookmarklet.
+     * @param {HttpGet|Mailto} url - object representing the location action to get
+     * @param {calback} [action] - optional callback handling url string (defaults to load in current browser window)
+     */
+    function get( url, action ) {
+        action = action || function ( loc ) { window.location.href = loc; };
+        var location = '';
+
+        // Concatenate the url base
+        if ( wtf.ARRAY( url.base ) ) {
+            var bases = [];
+            for ( var b = 0; b < url.base.length; b++ ) {
+                bases.push( flatten( url.base[b] ) );
+            }
+            location += bases.join( ',' );
+        } else {
+        location += flatten( url.base );
         }
-      }
-      location += flat.join( '&' );
+
+        // Concatenate the url query parameters
+        if ( url.parameters.length > 0 ) {
+            location += '?';
+            var flat = [];
+            for ( var l = 0; l < url.parameters.length; l++ ) {
+                for ( var key in url.parameters[l] ) {
+                    if ( url.parameters[l].hasOwnProperty( key ) ) {
+                        flat.push( key + '=' + flatten( url.parameters[l][key] ) );
+                    }
+                }
+            }
+            location += flat.join( '&' );
+        }
+
+        // Concatenate the anchor / hash
+        if ( url.anchor ) location += ( '#' + flatten( url.anchor ) );
+
+        // Do what's specified with that url location
+        action( location );
     }
 
-    if ( url.anchor ) location += ( '#' + flatten( url.anchor ) );
+    /**
+     * Constructs an object representing the fields of an URL/URI
+     * @param {string|string[]} baseUrl - array of recipient email address
+     * @param {object[]} parameters - array of key = value pairs, values may be numbers, strings, functions or arrays of the same
+     * @param {string} anchor - string of anchor HTML node (*NOTE omit # symbol, it is automatically prepended)
+     * @returns {object}
+     */
+    function HttpGet( baseUrl, parameters, anchor ) {
+        if ( wtf.STRING( baseUrl ) ) baseUrl = [].push( baseUrl );
+        if ( !parameters ) parameters = [];
+        if ( !anchor ) anchor = '';
 
-    action( location );
-  }
+        return {
+            'base' : baseUrl,
+            'parameters' : parameters,
+            'anchor' : anchor
+        };
+    }
 
-  function Mailto( recipients, parameters ) {
-    if ( recipients && recipients.length > 0 ) recipients[0] = 'mailto:' + recipients[0];
-    if ( !parameters ) parameters = [];
+    /**
+     * Constructs an object representing the fields of a 'mailto:'
+     * @param {string|string[]} to - array of recipient email address
+     * @param {object[]} parameters - array of key = value pairs, values may be numbers, strings, functions or arrays of the same
+     * @returns {object}
+     */
+    function Mailto( to, parameters ) {
+        // Scrub arguments into a arrays, and prepend mailto protocol
+        if ( wtf.STRING( to ) ) to = [].push( to );
+        to[0] = 'mailto:' + to[0];
+        if ( !parameters ) parameters = [];
+
+        return HttpGet( to, parameters );
+    }
+
+    /**
+     * Gets the author of an HTML document if present.
+     * @returns {string}
+     */
+    function author() {
+        var meta = document.head.getElementsByTagName('meta');
+        for ( var m = 0; m < meta.length; m++ ) {
+            if ( meta[m].name === 'author' ) return meta[m].content;
+        }
+        return '';
+    }
+
+    /**
+     * Gets the lastModified date of an HTML document if present.
+     * @returns {string}
+     */
+    function lastModified() { return document.lastModified; }
+
+    /**
+     * Gets the selected (highlighted) text from an HTML document if present.
+     * @returns {string}
+     */
+    function selectedText() {
+        var text = '';
+        if ( window.getSelection ) {
+            text = window.getSelection().toString();
+        } else if (
+            document.selection
+            && document.selection.type != 'Control'
+        ) {
+            text = document.selection.createRange().text;
+        }
+        return text;
+    }
+
+    /**
+     * Gets the title of an HTML document if present.
+     * @returns {string}
+     */
+    function title () { return document.title; }
+
+    /**
+     * Gets the URL of an HTML document if present.
+     * @returns {string}
+     */
+    function url() { return document.URL; }
 
     return {
-      'base' : recipients,
-      'parameters' : parameters,
-      'anchor' : null
+        'get' : get,
+        'Mailto' : Mailto,
+        'HttpGet' : HttpGet,
+
+        'author' : author,
+        'lastModified' : lastModified,
+        'selectedText' : selectedText,
+        'title' : title,
+        'url' : url
     };
-  }
-
-  function HttpGet( baseUrl, parameters, anchor ) {
-    if ( !parameters ) parameters = [];
-
-    return {
-      'base' : baseUrl,
-      'parameters' : parameters,
-      'anchor' : anchor
-    };
-  }
-
-  function author() {
-    var meta = document.head.getElementsByTagName('meta');
-    for ( var m = 0; m < meta.length; m++ ) {
-      if ( meta[m].name === 'author' ) return meta[m].content;
-    }
-    return '';
-  }
-
-  function lastModified() { return document.lastModified; }
-
-  function selectedText() {
-    var text = '';
-    if ( window.getSelection ) {
-      text = window.getSelection().toString();
-    } else if (
-      document.selection
-      && document.selection.type != 'Control'
-    ) {
-      text = document.selection.createRange().text;
-    }
-    return text;
-  }
-
-  function title () { return document.title; }
-
-  function url() { return document.URL; }
-
-  return {
-    'get' : get,
-    'Mailto' : Mailto,
-    'HttpGet' : HttpGet,
-
-    'author' : author,
-    'lastModified' : lastModified,
-    'selectedText' : selectedText,
-    'title' : title,
-    'url' : url
-  };
-
 } )();
